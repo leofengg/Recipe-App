1b0cd6cd0b7500e3f13e82516d83d1ea99b7078a
leofengg <leofengg02@gmail.com>
2023-08-05 22:48:56 -0600

    finished registration. can register on webapp now

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,9 +4,8 @@ export const Navbar = () => {
 
     return <div className='navbar'>
         <Link to="/"> Home</Link>
-        <Link to="/auth"> Login/Register</Link>
         <Link to="/create-recipe"> Create Recipe</Link>
         <Link to="/save-recipe"> Save Recipe</Link>
-        
+        <Link to="/auth"> Login/Register</Link>
     </div>
 }
