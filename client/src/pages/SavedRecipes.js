08fc132a538ad39bf40181dfae7e17b50e87e5cb
leofengg <leofengg02@gmail.com>
2023-08-05 21:03:21 -0600

    worked on front end, got navbar and links done

---
+++ client/src/pages/SavedRecipes.js
@@ -0,0 +1,3 @@
+export const SavedRecipies = () => {
+    return <div>Saved</div>
+}
--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -6,13 +6,16 @@ import { UserModel } from '../models/Users.js';
 const router = express.Router();
 
 router.post('/register', async(req, res) => {
+    //get username/pw from request
     const {username, password} = req.body;
     const user = await UserModel.findOne({username: username});
 
+    //check if username is already in database
     if (user) {
         return res.json({message: "User already exists."});
     }
 
+    //hash password
     const hashedPw = await bcrypt.hash(password, 10);
     const newUser = new UserModel({username, password: hashedPw});
     await newUser.save();
@@ -21,13 +24,16 @@ router.post('/register', async(req, res) => {
 });
 
 router.post('/login', async(req, res) => {
+    //get username/pw from request
     const {username, password} = req.body;
     const user = await UserModel.findOne({username});
 
+    //check if user exists 
     if (!user) {
         return res.json({message: "User doesn't exist"});
     }
 
+    //check for password validity
     const isPasswordValid = await bcrypt.compare(password, user.password);
 
     if (!isPasswordValid) {
