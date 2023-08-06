e42f8b672441f9076eb3f8c022523929a6709731
leofengg <leofengg02@gmail.com>
2023-08-06 14:44:01 -0600

    finished home page, 2 bugs image won't load and cannot add more than 1 recipe

--- client/src/pages/Create-Recipe.js
+++ client/src/pages/Create-Recipe.js
@@ -1,72 +1,75 @@
-// import { useState } from "react"
-// import axios from 'axios'
-// import { useGetUserID } from "../hooks/useGetUserID";
+import { useState } from "react"
+import {useNavigate} from 'react-router-dom'
+import axios from 'axios'
+import { useGetUserID } from "../hooks/useGetUserID";
 
-// export const CreateRecipe = () => {
+export const CreateRecipe = () => {
 
-//     const userID = useGetUserID();
+    const userID = useGetUserID();
 
-//     const [recipe, setRecipe] = useState({
-//         name: '',
-//         ingredients: [],
-//         instructions: '',
-//         imageUrl: '',
-//         cookingTime: 0,
-//         userOwner: userID
-//     });
+    const [recipe, setRecipe] = useState({
+        name: '',
+        ingredients: [],
+        instructions: '',
+        imageUrl: '',
+        cookingTime: 0,
+        userOwner: userID
+    });
 
-    
+    const navigate = useNavigate();
 
-//     const handleChange = (event) => {
-//         const {name, value} = event.target; 
-//         setRecipe({...recipe, [name]: value})
-//     }
-//     const handleIngredientChange = (event, idx) => {
-//         const {value} = event.target;
-//         const ingredients = [...recipe.ingredients];
-//         ingredients[idx] = value;
-//         setRecipe({...recipe, ingredients})
-//     }
+    const handleChange = (event) => {
+        const {name, value} = event.target; 
+        setRecipe({...recipe, [name]: value})
+    }
+    const handleIngredientChange = (event, idx) => {
+        const {value} = event.target;
+        const ingredients = [...recipe.ingredients];
+        ingredients[idx] = value;
+        setRecipe({...recipe, ingredients})
+    }
 
-//     const onSubmit = async (event) => {
-//         event.preventDefault();
+    const onSubmit = async (event) => {
+        event.preventDefault();
 
-//         try {
-//             await axios.post('http://localhost:5000/recipes', recipe);
-//             alert("Recipe Created")
-//         } catch (err) {
-//             console.error(err)
-//         }
-//     }
-//     const addIngredient = () => {
-//         setRecipe({...recipe, ingredients: [...recipe.ingredients, '']})
-//     }
+        try {
+            await axios.post('http://localhost:5000/recipes', recipe);
+            alert("Recipe Created")
+            navigate('/')
+        } catch (err) {
+            console.error(err)
+        }
+    }
+    const addIngredient = () => {
+        setRecipe({...recipe, ingredients: [...recipe.ingredients, '']})
+    }
 
-//     return <div className="create-recipe">
-//         <h2>Create Recipe</h2>
-//         <form onSubmit={onSubmit}>
-//             <label htmlFor="name"> Name</label>
-//             <input type="text" id="name" name="name" onChange={handleChange}/>
+    return <div className="create-recipe">
+        <h2>Create Recipe</h2>
+        <form onSubmit={onSubmit}>
+            <label htmlFor="name"> Name</label>
+            <input type="text" id="name" name="name" onChange={handleChange}/>
 
-//             <label htmlFor="description">Description</label>
-//             <textarea id="description" name="description" onChange={handleChange}></textarea>
+            <label htmlFor="description">Description</label>
+            <textarea id="description" name="description" onChange={handleChange}></textarea>
 
-//             <label htmlFor="ingredients">Ingredients</label>
-//             {recipe.ingredients.map((ingredients, idx) => (
-//                 <input key={idx} type="text" name="ingredients" value={ingredients} onChange={(event) => handleIngredientChange(event, idx)}></input>
-//             ))}
-//             <button onClick={addIngredient} type="button"> Add Ingredient</button>
+            <label htmlFor="ingredients">Ingredients</label>
+            {recipe.ingredients.map((ingredients, idx) => (
+                <input key={idx} type="text" name="ingredients" value={ingredients} onChange={(event) => handleIngredientChange(event, idx)}></input>
+            ))}
+            <button onClick={addIngredient} type="button"> Add Ingredient</button>
 
-//             <label htmlFor="instructions">Instructions</label>
-//             <textarea id="instructions" name="instructions" onChange={handleChange}></textarea>
+            <label htmlFor="instructions">Instructions</label>
+            <textarea id="instructions" name="instructions" onChange={handleChange}></textarea>
 
-//             <label htmlFor="imageUrl">Image URL</label>
-//             <input type="text" id="imageUrl" name="imageUrl" onChange={handleChange}></input>
+            <label htmlFor="imageUrl">Image URL</label>
+            <input type="text" id="imageUrl" name="imageUrl" onChange={handleChange}></input>
 
-//             <label htmlFor="cookingTime">Cooking Time (minutes)</label>
-//             <input type="number" id="cookingTime" name="cookingTime" onChange={handleChange}></input>
+            <label htmlFor="cookingTime">Cooking Time (minutes)</label>
+            <input type="number" id="cookingTime" name="cookingTime" onChange={handleChange}></input>
 
-//             <button type="submit">Create Recipe</button>
-//         </form>
-//         </div>
-// }
+            <button type="submit">Create Recipe</button>
+        </form>
+        </div>
+}
+
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,3 +1,38 @@
+import { useEffect, useState } from "react"
+import axios from 'axios'
+
 export const Home = () => {
-    return <div>Home</div>
+    const [recipes, setRecipes] = useState([])
+
+    useEffect(() => {
+        const fetchRecipe = async () => {
+            try {
+                const response = await axios.get('http://localhost:5000/recipes');
+                setRecipes(response.data);
+                console.log(response.data)
+            } catch (err) {
+                console.error(err)
+            }
+        }
+        fetchRecipe();
+    }, [])
+    return <div>
+        <h1>Recipes</h1>
+        <ul>
+            {recipes.map((recipe) => (
+                <li key={recipe._id}>
+                    <div>
+                        <h2>
+                            {recipe.name}
+                        </h2>
+                    </div>
+                    <div className="instructions">
+                        <p>{recipe.instructions}</p>
+                    </div>
+                    <img src={recipe.imageUrl} alt={recipe.name} />
+                    <p> Cooking Time: {recipe.cookingTime} (minutes)</p>
+                </li>
+            ))}
+        </ul>
+    </div> 
 }
