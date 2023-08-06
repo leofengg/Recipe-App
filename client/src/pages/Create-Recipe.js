f612d04129d0547304c7652fb591416aaff277c8
leofengg <leofengg02@gmail.com>
2023-08-06 11:57:03 -0600

    almost done create recipe in front end

--- client/src/pages/Create-Recipe.js
+++ client/src/pages/Create-Recipe.js
@@ -1,3 +1,54 @@
+import { useState } from "react"
+
 export const CreateRecipe = () => {
-    return <div>Create</div>
+
+    const [recipe, setRecipe] = useState({
+        name: '',
+        ingredients: [],
+        instructions: '',
+        imageUrl: '',
+        cookingTime: 0,
+        userOwner: 0
+    });
+
+    const handleChange = (event) => {
+        const {name, value} = event.target; 
+        setRecipe({...recipe, [name]: value})
+    }
+    const handleIngredientChange = (event, idx) => {
+        const {value} = event.target;
+        const ingredients = [...recipe.ingredients];
+        ingredients[idx] = value;
+        setRecipe({...recipe, ingredients})
+    }
+
+    const addIngredient = () => {
+        setRecipe({...recipe, ingredients: [...recipe.ingredients, '']})
+    }
+
+    return <div className="create-recipe">
+        <h2>Create Recipe</h2>
+        <form>
+            <label htmlFor="name"> Name</label>
+            <input type="text" id="name" name="name" onChange={handleChange}/>
+
+            <label htmlFor="description">Description</label>
+            <textarea id="description" name="description" onChange={handleChange}></textarea>
+
+            <label htmlFor="ingredients">Ingredients</label>
+            {recipe.ingredients.map((ingredients, idx) => (
+                <input key={idx} type="text" name="ingredients" value={ingredients} onChange={(event) => handleIngredientChange(event, idx)}></input>
+            ))}
+            <button onClick={addIngredient}> Add Ingredient</button>
+
+            <label htmlFor="instructions">Instructions</label>
+            <textarea id="instructions" name="instructions" onChange={handleChange}></textarea>
+
+            <label htmlFor="imageUrl">Image URL</label>
+            <input type="text" id="imageUrl" name="imageUrl" onChange={handleChange}></input>
+
+            <label htmlFor="cookingTime">Cooking Time (minutes)</label>
+            <input type="number" id="cookingTime" name="cookingTime" onChange={handleChange}></input>
+        </form>
+        </div>
 }
--- server/src/routes/recipes.js
+++ server/src/routes/recipes.js
@@ -2,6 +2,7 @@ import { RecipeModel } from "../models/Recipes.js";
 
 import express from 'express'
 import mongoose from "mongoose";
+import { UserModel } from "../models/Users.js";
 
 
 const router = express.Router();
@@ -25,6 +26,40 @@ router.post('/', async (req, res) => {
         res.json(response);
     } catch (err) {
         res.json(err);
+    }
+})
+
+router.put('/', async (req, res) => {
+
+    try {
+        const recipe = await RecipeModel.findById(req.body.recipeID);
+        const user = await UserModel.findById(req.body.userID);
+        user.savedRecipes.push(recipe);
+        await user.save();
+        res.json({savedRecipes: user.savedRecipes})
+    } catch (err) {
+        res.json(err);
+    }
+})
+
+router.get('/savedRecipes/ids', async (req, res) => {
+    try {
+        const user = await UserModel.findById(req.body.userID)
+        res.json({savedRecipes: user?.savedRecipes})
+    } catch (err) {
+        res.json(err)
+    }
+})
+
+router.get('/savedRecipes', async (req, res) => {
+    try {
+        const user = await UserModel.findById(req.body.userID)
+        const savedRecipes = await RecipeModel.find({
+            _id: {$in: user.savedRecipes}
+        })
+        res.json({savedRecipes})
+    } catch (err) {
+        res.json(err)
     }
 })
 
