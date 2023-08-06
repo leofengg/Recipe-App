d5228eabca232175460a7a5aa42ccd298ba18b90
leofengg <leofengg02@gmail.com>
2023-08-06 11:27:54 -0600

    made recipe schema, able to post and get from api

--- server/src/index.js
+++ server/src/index.js
@@ -3,12 +3,14 @@ import cors from 'cors'; //set up rules to communicate between front and back
 import mongoose from 'mongoose'; //speak to mongodb
 
 import {userRouter} from './routes/users.js'
+import { recipeRouter } from './routes/recipes.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use('/auth', userRouter);
+app.use('/recipes', recipeRouter);
 
 mongoose.connect( 'mongodb+srv://leofengg02:Yutwimlar68MJh6r@cluster0.9my7rzx.mongodb.net/?retryWrites=true&w=majority' );
 
