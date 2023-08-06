4e8dbec08c16756eee54f4a7c144f8edbd917e79
leofengg <leofengg02@gmail.com>
2023-08-06 13:39:36 -0600

    post request for new recipe broke

--- client/src/pages/Create-Recipe.js
+++ client/src/pages/Create-Recipe.js
@@ -1,54 +1,72 @@
-import { useState } from "react"
+// import { useState } from "react"
+// import axios from 'axios'
+// import { useGetUserID } from "../hooks/useGetUserID";
 
-export const CreateRecipe = () => {
+// export const CreateRecipe = () => {
 
-    const [recipe, setRecipe] = useState({
-        name: '',
-        ingredients: [],
-        instructions: '',
-        imageUrl: '',
-        cookingTime: 0,
-        userOwner: 0
-    });
+//     const userID = useGetUserID();
 
-    const handleChange = (event) => {
-        const {name, value} = event.target; 
-        setRecipe({...recipe, [name]: value})
-    }
-    const handleIngredientChange = (event, idx) => {
-        const {value} = event.target;
-        const ingredients = [...recipe.ingredients];
-        ingredients[idx] = value;
-        setRecipe({...recipe, ingredients})
-    }
+//     const [recipe, setRecipe] = useState({
+//         name: '',
+//         ingredients: [],
+//         instructions: '',
+//         imageUrl: '',
+//         cookingTime: 0,
+//         userOwner: userID
+//     });
 
-    const addIngredient = () => {
-        setRecipe({...recipe, ingredients: [...recipe.ingredients, '']})
-    }
+    
 
-    return <div className="create-recipe">
-        <h2>Create Recipe</h2>
-        <form>
-            <label htmlFor="name"> Name</label>
-            <input type="text" id="name" name="name" onChange={handleChange}/>
+//     const handleChange = (event) => {
+//         const {name, value} = event.target; 
+//         setRecipe({...recipe, [name]: value})
+//     }
+//     const handleIngredientChange = (event, idx) => {
+//         const {value} = event.target;
+//         const ingredients = [...recipe.ingredients];
+//         ingredients[idx] = value;
+//         setRecipe({...recipe, ingredients})
+//     }
 
-            <label htmlFor="description">Description</label>
-            <textarea id="description" name="description" onChange={handleChange}></textarea>
+//     const onSubmit = async (event) => {
+//         event.preventDefault();
 
-            <label htmlFor="ingredients">Ingredients</label>
-            {recipe.ingredients.map((ingredients, idx) => (
-                <input key={idx} type="text" name="ingredients" value={ingredients} onChange={(event) => handleIngredientChange(event, idx)}></input>
-            ))}
-            <button onClick={addIngredient}> Add Ingredient</button>
+//         try {
+//             await axios.post('http://localhost:5000/recipes', recipe);
+//             alert("Recipe Created")
+//         } catch (err) {
+//             console.error(err)
+//         }
+//     }
+//     const addIngredient = () => {
+//         setRecipe({...recipe, ingredients: [...recipe.ingredients, '']})
+//     }
 
-            <label htmlFor="instructions">Instructions</label>
-            <textarea id="instructions" name="instructions" onChange={handleChange}></textarea>
+//     return <div className="create-recipe">
+//         <h2>Create Recipe</h2>
+//         <form onSubmit={onSubmit}>
+//             <label htmlFor="name"> Name</label>
+//             <input type="text" id="name" name="name" onChange={handleChange}/>
 
-            <label htmlFor="imageUrl">Image URL</label>
-            <input type="text" id="imageUrl" name="imageUrl" onChange={handleChange}></input>
+//             <label htmlFor="description">Description</label>
+//             <textarea id="description" name="description" onChange={handleChange}></textarea>
 
-            <label htmlFor="cookingTime">Cooking Time (minutes)</label>
-            <input type="number" id="cookingTime" name="cookingTime" onChange={handleChange}></input>
-        </form>
-        </div>
-}
+//             <label htmlFor="ingredients">Ingredients</label>
+//             {recipe.ingredients.map((ingredients, idx) => (
+//                 <input key={idx} type="text" name="ingredients" value={ingredients} onChange={(event) => handleIngredientChange(event, idx)}></input>
+//             ))}
+//             <button onClick={addIngredient} type="button"> Add Ingredient</button>
+
+//             <label htmlFor="instructions">Instructions</label>
+//             <textarea id="instructions" name="instructions" onChange={handleChange}></textarea>
+
+//             <label htmlFor="imageUrl">Image URL</label>
+//             <input type="text" id="imageUrl" name="imageUrl" onChange={handleChange}></input>
+
+//             <label htmlFor="cookingTime">Cooking Time (minutes)</label>
+//             <input type="number" id="cookingTime" name="cookingTime" onChange={handleChange}></input>
+
+//             <button type="submit">Create Recipe</button>
+//         </form>
+//         </div>
+// }
--- server/src/routes/recipes.js
+++ server/src/routes/recipes.js
@@ -23,44 +23,45 @@ router.post('/', async (req, res) => {
 
     try {
         const response = await recipe.save();
+        console.log(response)
         res.json(response);
     } catch (err) {
         res.json(err);
     }
 })
 
-router.put('/', async (req, res) => {
+// router.put('/', async (req, res) => {
 
-    try {
-        const recipe = await RecipeModel.findById(req.body.recipeID);
-        const user = await UserModel.findById(req.body.userID);
-        user.savedRecipes.push(recipe);
-        await user.save();
-        res.json({savedRecipes: user.savedRecipes})
-    } catch (err) {
-        res.json(err);
-    }
-})
+//     try {
+//         const recipe = await RecipeModel.findById(req.body.recipeID);
+//         const user = await UserModel.findById(req.body.userID);
+//         user.savedRecipes.push(recipe);
+//         await user.save();
+//         res.json({savedRecipes: user.savedRecipes})
+//     } catch (err) {
+//         res.json(err);
+//     }
+// })
 
-router.get('/savedRecipes/ids', async (req, res) => {
-    try {
-        const user = await UserModel.findById(req.body.userID)
-        res.json({savedRecipes: user?.savedRecipes})
-    } catch (err) {
-        res.json(err)
-    }
-})
+// router.get('/savedRecipes/ids', async (req, res) => {
+//     try {
+//         const user = await UserModel.findById(req.body.userID)
+//         res.json({savedRecipes: user?.savedRecipes})
+//     } catch (err) {
+//         res.json(err)
+//     }
+// })
 
-router.get('/savedRecipes', async (req, res) => {
-    try {
-        const user = await UserModel.findById(req.body.userID)
-        const savedRecipes = await RecipeModel.find({
-            _id: {$in: user.savedRecipes}
-        })
-        res.json({savedRecipes})
-    } catch (err) {
-        res.json(err)
-    }
-})
+// router.get('/savedRecipes', async (req, res) => {
+//     try {
+//         const user = await UserModel.findById(req.body.userID)
+//         const savedRecipes = await RecipeModel.find({
+//             _id: {$in: user.savedRecipes}
+//         })
+//         res.json({savedRecipes})
+//     } catch (err) {
+//         res.json(err)
+//     }
+// })
 
 export {router as recipeRouter};
