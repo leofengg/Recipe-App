2afdfa723d1cbba4a0948108f71c020bde4e4742
leofengg <leofengg02@gmail.com>
2023-08-07 15:08:50 -0600

    fixed saved recipies page

--- client/src/pages/SavedRecipes.js
+++ client/src/pages/SavedRecipes.js
@@ -4,30 +4,21 @@ import { useGetUserID } from "../hooks/useGetUserID"
 
 
 export const SavedRecipes = () => {
-    const [recipes, setRecipes] = useState([]);
+
     const [savedRecipes, setSavedRecipes] = useState([]);
     const userID = useGetUserID();
 
     useEffect(() => {
-        const fetchRecipe = async () => {
-            try {
-                const response = await axios.get('http://localhost:5000/recipes');
-                setRecipes(response.data);
-            } catch (err) {
-                console.error(err)
-            }
-        }
 
         const fetchSavedRecipe = async () => {
             try {
-                const response = await axios.get(`http://localhost:5000/recipes/savedRecipes/ids/${userID}`);
+                const response = await axios.get(`http://localhost:5000/recipes/savedRecipes/${userID}`);
                 setSavedRecipes(response.data.savedRecipes)
                 console.log(response.data.saveRecipe)
             } catch (err) {
                 console.error(err)
             }
         }
-
 
         fetchSavedRecipe();
 
@@ -44,10 +35,24 @@ export const SavedRecipes = () => {
         }
     }
 
-    const isRecipeSaved = (id) => savedRecipes.includes(id);
 
     return <div>
         <h1>Recipes</h1>
-        
+        <ul>
+            {savedRecipes.map((recipe)=> (
+                <li key={recipe._id}>
+                    <div>
+                        <h2>
+                            {recipe.name}
+                        </h2>
+                    </div>
+                    <div className="instructions">
+                        <p>{recipe.instructions}</p>
+                    </div>
+                    <img src={recipe.imageUrl} alt={recipe.name} />
+                    <p> Cooking Time: {recipe.cookingTime} (minutes)</p>
+                </li>
+            ))}
+        </ul>
     </div> 
 }
